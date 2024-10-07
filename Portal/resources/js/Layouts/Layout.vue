0085ad1b6aa98b69f8594a00fbc6738cfdd6d039
bOpili <patrykwawrzak53@gmail.com>
2024-10-07 21:03:14 +0200

    1.0

---
+++ Portal/resources/js/Layouts/Layout.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <Head>
+        <title>Portal</title>
+    </Head>
+    <div>
+        <header class="bg-indigo-500 text-white">
+            <nav class="flex items-center justify-between p-4 max-w-screen-lg mx-auto">
+                <div class="space-x-6">
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                </div>
+            </nav>
+        </header>
+        <main class="p-4">
+            <slot />
+        </main>
+    </div>
+</template>
